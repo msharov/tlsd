7b0a99aef76380ffd3a09ed6a3c67cc257090a4a
Mike Sharov <msharov@users.sourceforge.net>
2014-12-14 11:48:36 -0500

    Init and link to OpenSSL

--- tlsd.c
+++ tlsd.c
@@ -4,11 +4,20 @@
 // This file is free software, distributed under the MIT License.
 
 #include "config.h"
+#if !HAVE_OPENSSL_BIO_H || !HAVE_OPENSSL_SSL_H || !HAVE_OPENSSL_ERR_H
+    #error "tlsd requires the OpenSSL library"
+#endif
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (void)
 {
+    SSL_load_error_strings();
+    ERR_load_BIO_strings();
+    OpenSSL_add_all_algorithms();
     puts ("Hello world!");
     return EXIT_SUCCESS;
 }
